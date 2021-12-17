6d10fec2552a75c36f20361f4536237fbc2f0b30
Ernesto905 <enriquez906@yahoo.com>
2021-12-16 19:24:10 -0500

    Removed functions for local gate transformation
    Cleaned some stuff up
    
    Purpose:
    The original plan was to rotate the Bloch vector via matrix multiplication using the system numeric namespace's inbuilt matrices.
    
    After further consideration, matrix multiplication will be handled through a python script using NumPy's
    n-dimensional arrays.

--- Assets/Scripts/MyBloch.cs
+++ Assets/Scripts/MyBloch.cs
@@ -13,14 +13,16 @@ public class MyBloch : MonoBehaviour
     UnityEngine.Quaternion defaultRotation;
     public GameObject rotator;
 
-    [SerializeField] private Complex waveFunction = Complex.Zero;
-    [SerializeField] private Complex euler = Complex.Zero;
-    [SerializeField] private float theta = 0;
-    [SerializeField] private float phi = 0;
+
+
+    
 
     public static float y=0f;
     public static float x=0f;
     public static float z=0f;
+
+   
+
 
 
 
@@ -31,38 +33,14 @@ public class MyBloch : MonoBehaviour
     {   
         //used to determine starting pos of sphere
         BSphereStartPos =  transform.localPosition;
-        Debug.Log(theta);
-       
     }
 
-    // Update is called once per frame
     void Update()
     {
-        DateTime currentTime = DateTime.Now;
-        float secondsDegree = -(currentTime.Second / 60f) * 360f;
-        
-        //test quaternion 90 degrees Y axis
-        //UnityEngine.Quaternion rotation = UnityEngine.Quaternion.Euler();         not currently being used
-    
-        //Perform roation on sphere (Time Dependent)
-        rotator = GameObject.Find("Rotator");
-        //rotator.transform.localRotation = UnityEngine.Quaternion.Euler(new Vector3(0,0,secondsDegree));
 
-
-        //rotator.transform.localRotation = UnityEngine.Quaternion.Euler(rotationVector);
-    
-        //Debug.Log("current orientation is: " + rotationVector);
-        
+        //rotateRot(thetaInDeg);                            THIS WILL BE PLACEMENT OF SOCKKET VARIABLE   
 
     }
-
-    void calcWF()
-    {
-
-        waveFunction = new Complex(Mathf.Cos(theta / 2), (Mathf.Sin(theta / 2)));
-
-    }
-
 
     
     //used for maintaining current orientation on entire Bsphere
@@ -76,4 +54,11 @@ public class MyBloch : MonoBehaviour
         transform.rotation = defaultRotation;
     }
 
+    
+    void rotateRot(int t)
+    {
+        rotator.transform.eulerAngles = new Vector3(0,0,t);
+    }
+
+    
 }
--- Assets/Scripts/X_Button.cs
+++ Assets/Scripts/X_Button.cs
@@ -1,32 +1,100 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using System;
-
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
 public class X_Button : MonoBehaviour
 {
     public GameObject rotator;
-
     [SerializeField] private float threshold= 0.1f;
     [SerializeField] private float deadZone = 0.025f;
  
     private bool _isPressed;
-    private Vector3 _startPos;
+    private UnityEngine.Vector3 _startPos;
     private ConfigurableJoint _joint; 
-
-
-
-
     public UnityEvent onPressed, onReleased;
-   
+
+    //Socket setup 
+    Thread mThread;
+    public string connectionIP = "127.0.0.1";
+    public int connectionPort = 25001;
+    IPAddress localAdd;
+    TcpListener listener;
+    TcpClient client;
+    Vector3 receivedPos = Vector3.zero;
+    bool running;
+
     void Start()
     {
         //basic button functionality
         _startPos = transform.localPosition;
         _joint = GetComponent<ConfigurableJoint>();
 
+        //commence socket connection
+        ThreadStart ts = new ThreadStart(GetInfo);
+        mThread = new Thread(ts);
+        mThread.Start();
 
+
+    }
+    void GetInfo()
+    {
+        localAdd = IPAddress.Parse(connectionIP);
+        listener = new TcpListener(IPAddress.Any, connectionPort);
+        listener.Start();
+
+        client = listener.AcceptTcpClient();
+
+        running = true;
+        while (running)
+        {
+            SendAndReceiveData();
+        }
+        listener.Stop();
+    }
+
+
+    void SendAndReceiveData()
+    {
+        NetworkStream nwStream = client.GetStream();
+        byte[] buffer = new byte[client.ReceiveBufferSize];
+
+        //---receiving Data from the Host----
+        int bytesRead = nwStream.Read(buffer, 0, client.ReceiveBufferSize); //Getting data in Bytes from Python
+        string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead); //Converting byte data to string
+
+        if (dataReceived != null)
+        {
+            //---Using received data---
+            receivedPos = StringToVector3(dataReceived); //<-- assigning receivedPos value from Python
+            print("received pos data, and moved the Cube!");
+
+            //---Sending Data to Host----
+            byte[] myWriteBuffer = Encoding.ASCII.GetBytes("Hey I got your message Python! Do You see this massage?"); //Converting string to byte data
+            nwStream.Write(myWriteBuffer, 0, myWriteBuffer.Length); //Sending the data in Bytes to Python
+        }
+    }
+
+    public static Vector3 StringToVector3(string sVector)
+    {
+        // Remove the parentheses
+        if (sVector.StartsWith("(") && sVector.EndsWith(")"))
+        {
+            sVector = sVector.Substring(1, sVector.Length - 2);
+        }
+
+        // split the items
+        string[] sArray = sVector.Split(',');
+
+        // store as a Vector3
+        Vector3 result = new Vector3(
+            float.Parse(sArray[0]),
+            float.Parse(sArray[1]),
+            float.Parse(sArray[2]));
+
+        return result;
     }
 
     // Update is called once per frame
@@ -40,7 +108,7 @@ public class X_Button : MonoBehaviour
 
     private float GetValue()
     {
-        var value = Vector3.Distance(_startPos, transform.localPosition) / _joint.linearLimit.limit;
+        var value = UnityEngine.Vector3.Distance(_startPos, transform.localPosition) / _joint.linearLimit.limit;
 
         if (Math.Abs(value) < deadZone)
             value = 0;
@@ -66,14 +134,15 @@ public class X_Button : MonoBehaviour
         
         //configures rotator game object
         rotator = GameObject.Find("Rotator");
-        Transform rotate = rotator.transform;
-
-        
-        MyBloch.z-=180f;
-        UnityEngine.Vector3 rotation = new Vector3(MyBloch.x, MyBloch.y, MyBloch.z); //vector to rotate
-        
-
-        rotator.transform.eulerAngles = (rotation);
+        Transform rotate = rotator.transform;  
+        rotate.transform.Rotate(0,0,0); 
+        rotate.transform.Rotate(receivedPos);      
         
     }
+
+
+
+    
+
+    
 }
