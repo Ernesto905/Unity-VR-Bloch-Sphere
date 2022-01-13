5e568c3bdab9de94107146c014a5a05bf71a3045
Ernesto Enriquez <44984106+Ernesto905@users.noreply.github.com>
2022-01-12 19:42:47 -0500

    Added Measurement Button
    Qubit in superposition can now be measured. This measurement will collapse the qubit.
    TODO; Fix bug: Assuming qubit is initialized to 0 state, if a Hadamard transform is applied, followed by a Pauli Y gate, incorrect rotation occurs on the qubit

---
+++ Assets/Scripts/M_Button.cs
@@ -0,0 +1,130 @@
+using UnityEngine;
+using UnityEngine.Events;
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+public class M_Button : MonoBehaviour
+{
+    public GameObject rotator;
+    [SerializeField] private float threshold= 0.1f;
+    [SerializeField] private float deadZone = 0.025f;
+ 
+    private bool _isPressed;
+    private UnityEngine.Vector3 _startPos;
+    private ConfigurableJoint _joint; 
+    public UnityEvent onPressed, onReleased;
+    private string gate; 
+
+     
+    
+
+    Vector3 receivedPos = Vector3.zero;
+
+    void Start()
+    {
+        //basic button functionality
+        _startPos = transform.localPosition;
+        _joint = GetComponent<ConfigurableJoint>();
+
+    }
+    
+
+   
+
+    void sendConfirmation()
+    {
+
+        //---Establish network details----
+        NetworkStream nwStream = X_Button.client.GetStream();
+        byte[] buffer = new byte[X_Button.client.ReceiveBufferSize];
+
+        //---Send Confirmation---
+        byte[] myWriteBuffer = Encoding.ASCII.GetBytes(gate); //Converting string to byte data
+        nwStream.Write(myWriteBuffer, 0, myWriteBuffer.Length);
+
+        recieveConfirmation();        
+
+    }
+
+    void recieveConfirmation()
+    {
+        //---Establish network details----
+        NetworkStream nwStream = X_Button.client.GetStream();
+        byte[] buffer = new byte[X_Button.client.ReceiveBufferSize];
+
+        // //---receiving Data from the Host----
+        int bytesRead = nwStream.Read(buffer, 0, X_Button.client.ReceiveBufferSize); //Getting data in Bytes from Python
+        string dataReceived = Encoding.UTF8.GetString(buffer, 0, bytesRead); //Converting byte data to string
+
+        Debug.Log($"The data recieved via socket is {dataReceived}");
+        receivedPos = StringToVector3(dataReceived);
+    }
+    public static Vector3 StringToVector3(string sVector)
+    {
+        // Remove the parentheses
+        if (sVector.StartsWith("(") && sVector.EndsWith(")"))
+        {
+            sVector = sVector.Substring(1, sVector.Length - 2);
+        }
+
+        // split the items
+        string[] sArray = sVector.Split(',');
+
+        // store as a Vector3
+        Vector3 result = new Vector3(
+            float.Parse(sArray[0]),
+            float.Parse(sArray[1]),
+            float.Parse(sArray[2]));
+
+        return result;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if ( !_isPressed && GetValue() + threshold >= 1)
+            Pressed();
+        if (_isPressed && GetValue() - threshold <= 0)
+            Released();
+    }
+
+    private float GetValue()
+    {
+        var value = UnityEngine.Vector3.Distance(_startPos, transform.localPosition) / _joint.linearLimit.limit;
+
+        if (Math.Abs(value) < deadZone)
+            value = 0;
+
+        return Mathf.Clamp(value, -1f, 1f);  
+    }
+
+    private void Pressed()
+    {
+        _isPressed = true;
+        onPressed.Invoke();
+        zGate();
+    }
+
+    private void Released()
+    {
+        _isPressed = false;
+        onReleased.Invoke();
+    }
+
+    private void zGate()
+    { 
+        gate = "Measurement";
+        sendConfirmation();
+        
+
+        //Rotates Bloch Arrow 
+        Quaternion rotationAmt = Quaternion.Euler(receivedPos);
+        rotator = GameObject.Find("Rotator");
+        rotator.transform.localRotation = rotationAmt;
+              
+        
+    }
+    
+}
--- python/qiskitConnection.py
+++ python/qiskitConnection.py
@@ -1,17 +1,20 @@
-from qiskit import QuantumCircuit
+from qiskit import QuantumCircuit    
 from qiskit.quantum_info import Statevector
-from math import sqrt, pi
 import numpy as np
 import socket
 
-#initialize socket
-host, port = "127.0.0.1", 25001
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-sock.connect((host,port))
+    
 
-#initialize qbit
-currentState = [0,0,0] 
-qc = QuantumCircuit(1)
+def initializeSocket():
+    host, port = "127.0.0.1", 25001
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock.connect((host,port))
+    return sock
+
+def initializeCircuit():
+    currentState = [0,0,0] 
+    qc = QuantumCircuit(1)
+    return qc, currentState
 
 
 #takes in the alpha phase state. Normalize it, and returns it as degrees
@@ -32,51 +35,78 @@ def toPhi(t, b):
     return np.degrees(normalized_angle)
 
 
-while True:
-    recievedData = sock.recv(1024).decode("UTF-8") 
+def connectToUnity(sock, qc, currentState):
     
+    while True:
 
-    if recievedData == "XGate":
-        lastGate = "X Gate"
-        qc.x(0)
+        collapsed = False
+        recievedData = sock.recv(1024).decode("UTF-8") 
+
+        if recievedData == "XGate":
+            qc.x(0)
+            
+        elif recievedData == "hGate":
+            qc.h(0)
         
-    elif recievedData == "hGate":
-        lastGate = "H Gate"
-        qc.h(0)
-    
-    elif recievedData == "yGate":
-        lastGate = "Y Gate"
-        qc.y(0)
+        elif recievedData == "yGate":
+            qc.y(0)
+            
+        elif recievedData == "zGate":
+            qc.z(0)
+        elif recievedData == "Measurement":
+            collapsed = True
+            
+        else: 
+            raise Exception(f"Error: Recieved data unrecognized: {recievedData}")
         
-    elif recievedData == "zGate":
-        lastGate = "Z Gate"
-        qc.z(0)
         
-    else: 
-        raise Exception(f"Error: Recieved data unrecognized: {recievedData}")
-    
-    #qubit information to readable statevector
-    sv = Statevector(qc)
-    qstate = sv.data
-    
-    #Qubit properties 
-    alpha = qstate[0]
-    beta = qstate[1]
-    theta = toTheta(alpha)
-    phi = toPhi(theta, beta)
-    sv = Statevector(qc)
+        #get circuit as a readable statevector
+        sv = Statevector(qc)
+
+        #Measure in 0/1 basis 
+        if collapsed:
+            sv = sv.measure()[1]
+            if sv.data[0] == (0. + 0.j) and sv.data[1] == (1.+0.j):
+                print("Changed to ket 1 state")
+                qc.initialize('1')
+            elif sv.data[1] == (0. + 0.j) and sv.data[0] == (1.+0.j):
+                print("Changed to ket 0 state")
+                qc.initialize('0')
+        
+        #Establish qubit properties 
+        qstate = sv.data
+        alpha = qstate[0]
+        beta = qstate[1]
+        theta = int(toTheta(alpha))
+        phi = int(toPhi(theta, beta))
+        
+        
+        #alter the vector according to the new value of theta and phi
+        currentState[0] = theta
+        currentState[1] = phi
+        
+        #reset qubit's phase if in |0> state
+        if sv[0] != (1 + 0j) and theta == 0:
+            print('State reset')
+            qc = QuantumCircuit(1)
+            sv = Statevector(qc)
+            currentState = [0,0,0]
+
+             
+        print(f"Sent theta is {theta} and sent phi is {phi} and current statevector is {sv.data}")
+        posString = ','.join(map(str, currentState))
+        sock.sendall(posString.encode("UTF-8"))
+
+
+
+def main():
+    sock = initializeSocket()
+
+    circuit, currentState = initializeCircuit()
+
+    connectToUnity(sock, circuit, currentState)
 
     
-    
-    #alter the Vector according to the new value of theta
-    currentState[0] = int(theta)
-    currentState[1] = int(phi)
-    
-    posString = ','.join(map(str, currentState))
-    
-    print(f"Sent state is: {posString} and the sent gate is {lastGate}")
-    sock.sendall(posString.encode("UTF-8"))
-
-    
-
+if __name__ == '__main__':
+    main()
 
